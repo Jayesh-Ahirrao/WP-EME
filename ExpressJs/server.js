c638d874711ce8cbe94fb664fb6fe3a33109a563
Jayesh Ahirrao <ahirraojayesh88@gmail.com>
2024-11-25 18:28:39 +0530

    handling error

--- ExpressJs/server.js
+++ ExpressJs/server.js
@@ -34,60 +34,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// app.get('/student/:id', (req, res) => {
-//     const { id } = req.params;
-//     //fetch student data from database or any other source
-//     console.log(id);
-
-//     let studentData = {};
-
-//     // connection.query(`SELECT * FROM studentinfo WHERE studId=?`, [id], (err, result) => {
-//     //     if (!err) {
-//     //         studentData = result;
-//     //     }
-//     //     else {
-//     //         console.log(err);
-//     //     }
-//     // });
-
-
-
-//     connection.query(`SELECT * FROM studentinfo WHERE studId=?`, [id], (err, result) => {
-//         if (err) {
-//             console.error(err);
-//             res.status(500).json({ error: 'Database error' });
-//         } else {
-//             studentData = result[0]; // Assuming you want the first result
-//             console.log(studentData);
-//             res.json(studentData);
-//         }
-//     });
-
-//     console.log(studentData);
-
-
-//     res.json(studentData);
-// });
-
-
 app.get('/student/:id', (req, res) => {
     const { id } = req.params;
 
-    connection.query(`SELECT * FROM studentinfo WHERE studId=?`, [id], (err, result) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Database error' }); // Exit early on error
-        }
+    try {
+        connection.query(`SELECT * FROM studentinfo WHERE studId=?`, [id], (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Database error' }); // Exit early on error
+            }
 
-        if (result.length > 0) {
-            const studentData = result[0];
-            console.log(studentData);
-            res.json({ studentData });
-        } else {
-            res.json({ studentData: null });
-        }
-
-
-    });
+            if (result.length > 0) {
+                const studentData = result[0];
+                console.log(studentData);
+                res.json({ studentData });
+            } else {
+                res.json({ studentData: null });
+            }
+        });
+    } catch (err) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
